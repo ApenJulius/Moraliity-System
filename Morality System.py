5a9aee8b846e240698c8543a6ce38df9d99d1c70
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2020-10-18 00:19:04 +0200

    Update Morality System.py

--- Morality System.py
+++ Morality System.py
@@ -1,22 +1,20 @@
-morality = 0
-
 def moral(test):
     if test == "1":
         return 1
     elif test == "2":
         return -1
 
-test = input("You see someone by a cliff, do you:\n1.Warn them about getting too close\n2.Push them off\n")
-morality += moral(test)
+tests = [
+    "You see someone by a cliff, do you:\n1.Warn them about getting too close\n2.Push them off\n",
+    "A child drops its icecream, do you:\n1.Console the child\n2.Laugh and mock the child\n",
+    "You are given immunity and a gun, do you:\n1.Kill someone\n2.Not kill someone\n",
+    "You are given the cure to aids, do you:\n1.Cure aids\n2.Destroy the cure\n"
+]
 
-test = input("A child drops its icecream, do you:\n1.Console the child\n2.Laugh and mock the child\n")
-morality += moral(test)
-
-test = input("You are given immunity and a gun, do you:\n1.Kill someone\n2.Not kill someone\n")
-morality += moral(test)
-
-test = input("You are given the cure to aids, do you:\n1.Cure aids\n2.Destroy the cure\n")
-morality += moral(test)
+morality = 0
+for test in tests:
+    answer = input(test)
+    morality += moral(answer)
 
 if morality == -4:
     print("You absolute evil man")
